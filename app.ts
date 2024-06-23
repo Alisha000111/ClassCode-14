08c328f8159989ea2c024f7ee0f2041a76d21966
Alisha Muhammad Irfan <160208713+Alisha000111@users.noreply.github.com>
2024-06-23 16:05:22 +0500

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,63 @@
+//let makePizza
+//makePizza = "Your pizza is being prepared"
+//console.log(makePizza)
+//function makePizza(){
+//return "Korma"
+//}
+//let dish = makeKorma()
+//console.log(dish)
+//function makepizza(){
+    //return "Your pizza is being prepared"
+//}
+//let sirAsharibOrder = makePizza()
+    //console.log(sirAsharibOrder, "table 1")
+    //function makePizza(){
+        //return "Your pizza is being prepared"
+    //}
+    //let makeFrize = () => "Your pizzais being prepared"
+    //console.log(makeFrize(), "table 1")
+    //const helloPromise = new Promise((resolve) => {
+        //setTimeout(() => {
+            //resolve("Alisha")
+        //}, 2000)
+    //})
+    //helloPromise.then((message) => console.log(message)
+//)
+//const conditionalPromise = new Promise((resolve, reject) => {
+    //const success = Math.random() > 0.5;
+    //if (success) {
+        //resolve("success!");
+    //} else {
+        //reject(new Error("failure"));
+    //}
+//});
+//.then((result) => console.log (result))
+//.catch((error) => console.log(error. message));
+//function makepizza() {
+    //return "Alisha"
+//}
+//let makeFrize = () => {}
+//Promise
+//let sirZiaPromise = new Promise((res , rej) => {
+    //return rej( "passed out")
+//});
+//sirZiaPromise
+//.then((status)=> {
+//console.log(status);
+//})
+//.catch((err) => {
+//console.log(err);
+//})
+//async function checkSirZiaPromise(){
+//let promiseVal = await sirZiaPromise
+//console.log(promiseVal);
+//}
+//const helloPromise = new Promise
+//((resolve) => {
+//setTimeout(() => {
+ //   resolve("Alisha")
+//}, 10000)
+//})
+//helloPromise.then((message) =>
+//console.log(message)
+//)
